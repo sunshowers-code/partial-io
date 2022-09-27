1b7d75da47253058e86f8530c18590e29abd0429
sunshowers <rain@sunshowers.io>
2022-09-27 22:28:25 +0000

    Deploying to gh-pages from  @ cb909d25f933b5e98f629fe64a15dbccb7915cf4 🚀

--- rustdoc/implementors/core/borrow/trait.BorrowMut.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["bytes"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.64.0/core/borrow/trait.BorrowMut.html\" title=\"trait core::borrow::BorrowMut\">BorrowMut</a>&lt;[<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.64.0/std/primitive.u8.html\">u8</a>]&gt; for <a class=\"struct\" href=\"bytes/struct.BytesMut.html\" title=\"struct bytes::BytesMut\">BytesMut</a>","synthetic":false,"types":["bytes::bytes_mut::BytesMut"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
